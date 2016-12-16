20e0d985cffb9c9c250439568a48401b0e95236f
ivalentine <ian@ivalentine.net>
2016-12-15 21:26:11 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(message) {
+  return message.toUpperCase()
+}
+
+function whisper(message) {
+  return message.toLowerCase()
+}
+
+function logShout(message) {
+  console.log(message.toUpperCase())
+}
+
+function logWhisper(message) {
+  console.log(message.toLowerCase())
+}
